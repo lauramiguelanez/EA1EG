e37871fd6a9def4bc83f0158f53f822954583142
Laura Migueláñez <laura.miguelanez@gmail.com>
2020-03-14 17:19:49 +0100

    burgernav

--- client/src/components/bars/BurgerNav.js
+++ client/src/components/bars/BurgerNav.js
@@ -7,18 +7,21 @@ import '../../css/burgernav.scss';
 const BurgerNav = ({ page, setPage, setSearch }) => {
   const [openBurger, setOpenBurger] = useState(false);
   return (
-    <div className="burgernav-father">
+    <div className={`burgernav-father${openBurger ? ' openBurger' : ''}`}>
       {/* <NavLink className="nav-link" to="/">
         <img src="/img/logo.png" alt="EA1EG" className="logoSvg"></img>
       </NavLink> */}
       <button type="button" onClick={() => setOpenBurger(!openBurger)}>
-        <Burger />
+        {openBurger ? <img className="burgernav-image" src="/img/logo.png" alt="EA1EG"></img> : <Burger />}
       </button>
 
       {openBurger ? (
         <nav className="nav-style burgernav-wrapper" role="navigation">
-          <div className="" id="top-nav">
-            <div className="">
+          <div className="" id="top-burgernav">
+            <div className="burgernav-links">
+              <NavLink className="nav-link burger-link" to="/">
+                Home
+              </NavLink>
               <NavLink className="nav-link burger-link" to="/location">
                 Map
               </NavLink>
